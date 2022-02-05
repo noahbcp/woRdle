3dc5f53155915dec2703be2fce214f2ddc8c4dd2
Noah Piper <uqnpipe2@uq.edu.au>
2022-02-05 14:43:12 +1000

    old code

--- woRdle.R
+++ woRdle.R
@@ -1,41 +1,35 @@
 {
-## Clear environment
-rm(list=ls())
 ## Dependencies
+crayon <- require(crayon)
+if (crayon == FALSE) {install.packages('crayon')}
 library(crayon)
-## Load english 5 letter words
-acceptable <- read.csv('fiveletterwords.csv', fileEncoding="UTF-8-BOM")
-## Convert 'fiveletterwords.csv' into a list object
-wordlist <- read.csv('wordlist.csv', fileEncoding="UTF-8-BOM")
-## Sample wordlist to select goal word. Split goal word.
-## Unlist() is used to return a list of length = n letters
-goal_word <- sample(wordlist$wordlist, 1)
-goal_word <- strsplit(goal_word, split = "")
-goal_word <- as.list(unlist(goal_word))
-## Prompt user to guess
-guess1 <- readline(prompt = 'First guess: ')
-guess1 <- tolower(guess1)
-## Reject guesses longer than 5 characters
-while (nchar(guess1) != 5) {
-    guess1 <- readline(noquote('Please guess a 5 character word: '))
+## Clean environment; check for arguments. Use 'unlimit' for unlimited guesses.
+rm(list=ls())
+args <- commandArgs()
+## Load English 5 letter words
+acceptable <- read.csv('fiveletterwords.csv', fileEncoding = "UTF-8-BOM")
+wordlist <- read.csv('wordlist.csv', fileEncoding = 'UTF-8-BOM')
+## Find goal word
+actual <- sample(wordlist$wordlist, 1)
+## Limit guess count
+max_guess <- if ('unlimit' %in% args) {99999} else {6}
+guess_count <- 1
+while (guess_count == 1 & guess_count <= max_guess & guess_count + 1 ) {
+    guess <- readline(noquote(paste('Enter a 5 character word ','(guess ', guess_count, ' / ', max_guess, '):', sep = "")))
+    guess <- tolower(guess)
+    is_acceptable <- guess %in% acceptable$wordlist
+    ##Check that guesses are valid
+    if (nchar(guess) != 5) {
+        guess <- readline(noquote('You must enter a 5 character word: '))
     }
-## Reject guesses that are not real words
-is_acceptable <- guess1 %in% acceptable$wordlist
-while (is_acceptable != TRUE) {
-    guess1 <- readline(noquote('That word is not in the dictionary. Please guess again: '))
-    is_acceptable <- guess1 %in% acceptable$wordlist
-    while (nchar(guess1) != 5) {
-        guess1 <- readline(noquote('Please guess a 5 character word: '))
+    ## Reject guesses that are not real words
+    is_acceptable <- guess %in% acceptable$wordlist
+    if (is_acceptable != TRUE) {
+        guess <- readline(noquote('That word is not in the dictionary. Please guess again: '))
+        is_acceptable <- guess %in% acceptable$wordlist
+        if (nchar(guess) != 5) {
+            guess <- readline(noquote('You must enter a 5 character word: '))
         }
     }
-rm(is_acceptable)
-## Split guess string.
-## Unlist() is used to return a list of length = n letters
-guess1 <- strsplit(guess1, split = "")
-guess1 <- as.list(unlist(guess1))
-## Check if guess is in goal. Convert to matrix.
-guess1_results <- as.list(unlist(lapply(guess1, 'grepl', x = goal_word)))
-guess1_results_matrix <- matrix(guess1_results, nrow = 5, ncol = 5)
-correct_guesses <- which(guess1_results_matrix == TRUE, arr.ind = TRUE) ##row = matching letter in goal; col = letter in guess
-
+}
 }
