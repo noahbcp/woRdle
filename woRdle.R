a954bfb776eb67c14a48e6f8fc08fef7c124efad
Noah Piper <uqnpipe2@uq.edu.au>
2022-02-06 00:20:41 +1000

    Cleaned + annotated

--- woRdle.R
+++ woRdle.R
@@ -4,53 +4,55 @@ crayon <- require(crayon)
 if (crayon == FALSE) {install.packages('crayon')}
 library(crayon)
 ## Clean environment; check for arguments. Use 'unlimit' for unlimited guesses.
-rm(list=ls())
+rm(list = ls())
 args <- commandArgs()
-## Load English 5 letter words
+## Load English 5 letter words; selects goal word from 300 most common.
 acceptable <- read.csv('fiveletterwords.csv', fileEncoding = "UTF-8-BOM")
-wordlist <- read.csv('wordlist.csv', fileEncoding = 'UTF-8-BOM')
-## Find goal word
+wordlist <- read.csv('fiveletterwords.csv', fileEncoding = "UTF-8-BOM", nrows = 300)
 actual <- sample(wordlist$wordlist, 1)
+## Converts to raw to allow diffs to be calculated
 actual_raw <- charToRaw(actual)
 ## Limit guess count
 max_guess <- if ('unlimit' %in% args) {99999} else {6}
 guess_count <- 1
-## Create remaining set
-remaining <- as.raw(c(1:5))
+## Remaining vector serves to store letters not yet correctly guessed
+remaining <- actual_raw
+## Loops provided guess_count is less than max (default = 6)
 while (guess_count <= max_guess) {
     guess <- readline(noquote(paste('Enter a 5 character word ','(guess ', guess_count, ' / ', max_guess, '): ', sep = "")))
     guess <- tolower(guess)
     guess_raw <- charToRaw(guess)
     guess_count <- guess_count + 1
+    ## Checks to see if guess is 5 characters and a real word
     is_acceptable <- guess %in% acceptable$wordlist
     if (is_acceptable != TRUE) {
         guess_count <- guess_count - 1
         readline(noquote(paste('Sorry, that word is not acceptable.')))
-        guess <- readline(noquote(paste('Enter a 5 character word ','(guess ', guess_count, ' / ', max_guess, '): ', sep = "")))
-    }
+    } else {
     if (actual == guess) {
-        readline('You guessed right!')
         cat(bgGreen(guess))
+        readline('You guessed right!')
         opt <- options(show.error.messages = FALSE)
         on.exit(options(opt))
         stop()
     }
-    i = 0
-    while (i < length(actual_raw)) {
-        i <- (i + 1)
-        if (actual_raw[i] != guess_raw[i]) {
-            remaining[i] <- actual_raw[i]
-        }
-    }
+    ## i keeps track of the position in the word. i.e. for 'think', i[2] = 'h'
     i = 0
     while (i <= length(actual_raw)) {
         i <- (i + 1)
+        ## Checks whether the letter is in the correct position (i) then
+        ## whether the letter is in the word at all. Prints yellow
+        ## if letter is in word but wrong position otherwise prints red
+        ## if neither statement satisfied prints green as letter must be in
+        ## correct position
         if (actual_raw[i] != guess_raw[i]) {
             if (guess_raw[i] %in% remaining) {
-                cat(bgYellow(rawToChar(guess_raw[i])))
+                cat(bold(bgYellow(rawToChar(guess_raw[i]))))
                 remaining <- remaining[-i]
-            } else (cat(bgRed(rawToChar(guess_raw[i]))))
-        } else (cat(bgGreen(rawToChar(guess_raw[i]))))
+            } else (cat(bold(bgRed(rawToChar(guess_raw[i])))))
+        } else (cat(bold(bgGreen(rawToChar(guess_raw[i])))))
+    }
     }
 }
+continue <- readline('Oops! Better luck next time...')
 }
