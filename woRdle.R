f2b838351e765f0eb3aa5cd16f10eeba45c108ff
Noah Piper <uqnpipe2@uq.edu.au>
2022-02-05 14:48:56 +1000

    update

--- woRdle.R
+++ woRdle.R
@@ -15,20 +15,20 @@ actual <- sample(wordlist$wordlist, 1)
 max_guess <- if ('unlimit' %in% args) {99999} else {6}
 guess_count <- 1
 while (guess_count == 1 & guess_count <= max_guess & guess_count + 1 ) {
-    guess <- readline(noquote(paste('Enter a 5 character word ','(guess ', guess_count, ' / ', max_guess, '):', sep = "")))
+    guess <- readline(noquote(paste('Enter a 5 character word ','(guess ', guess_count, ' / ', max_guess, '): ', sep = "")))
     guess <- tolower(guess)
     is_acceptable <- guess %in% acceptable$wordlist
     ##Check that guesses are valid
     if (nchar(guess) != 5) {
-        guess <- readline(noquote('You must enter a 5 character word: '))
+        guess <- readline(noquote(paste('You must enter a 5 character word ', '(guess ', guess_count, ' / ', max_guess, '): ', sep = "")))
     }
     ## Reject guesses that are not real words
     is_acceptable <- guess %in% acceptable$wordlist
     if (is_acceptable != TRUE) {
-        guess <- readline(noquote('That word is not in the dictionary. Please guess again: '))
+        guess <- readline(noquote(paste('That word is not in the dictionary. Please guess again', '(guess ', guess_count, ' / ', max_guess, '): ', sep = "")))
         is_acceptable <- guess %in% acceptable$wordlist
         if (nchar(guess) != 5) {
-            guess <- readline(noquote('You must enter a 5 character word: '))
+            guess <- readline(noquote(paste('You must enter a 5 character word: ', '(guess ', guess_count, ' / ', max_guess, '): ', sep = "")))
         }
     }
 }
